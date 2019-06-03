ebf5af0061407ebdc54b15d4ef783b864e3ac213
judmorenomo <37850707+judmorenomo@users.noreply.github.com>
2019-06-03 18:26:49 -0500

    Shares function (#4)
    * Defined post as a react component
    
    * Shares working

--- src/components/ProfilePost.js
+++ src/components/ProfilePost.js
@@ -5,10 +5,9 @@ import CardContent from '@material-ui/core/CardContent'
 import Avatar from '@material-ui/core/Avatar'
 import Typography from '@material-ui/core/Typography'
 import red from '@material-ui/core/colors/red'
-import { withStyles } from '@material-ui/styles'
-import PropTypes from 'prop-types'
+import { makeStyles } from '@material-ui/core/styles'
 
-const styles = theme => ({
+const useStyles = makeStyles(theme => ({
   card: {
     width: 700,
     margin: 20
@@ -16,34 +15,31 @@ const styles = theme => ({
   avatar: {
     backgroundColor: red[500]
   }
-})
+}))
 
-class ProfilePost extends React.Component {  
-  render () {  
-    const { classes } = this.props  
-    return (
-      <Card className={classes.card}>
-        <CardHeader
-          avatar={
-            <Avatar className={classes.avatar} aria-label='Recipe'>
-              {this.props.user[0]}
-            </Avatar>
-          }
-          title={this.props.user}
-          subheader={this.props.date}
-        />
-        <CardContent>
-          <Typography variant='body2' color='textSecondary' component='p'>
-            {this.props.content}
-          </Typography>
-        </CardContent>
-      </Card>
-    )
-  }
+const ProfilePost = (props) => {  
+  const classes = useStyles()
+  const user = props.user
+  const date = props.date
+  const content = props.content
+  return (
+    <Card className={classes.card}>
+      <CardHeader
+        avatar={
+          <Avatar className={classes.avatar} aria-label='Recipe'>
+            {user[0]}
+          </Avatar>
+        }
+        title={user}
+        subheader={date}
+      />
+      <CardContent>
+        <Typography variant='body2' color='textSecondary' component='p'>
+          {content}
+        </Typography>
+      </CardContent>
+    </Card>
+  )
 }
 
-ProfilePost.propTypes = {
-  classes: PropTypes.object.isRequired
-}
-
-export default withStyles(styles)(ProfilePost)
+export default ProfilePost
