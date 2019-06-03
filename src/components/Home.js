cd5806c7ca3ce1ba088070783dab745d3dd1b449
Juan Diego Moreno <judmorenomo@unal.edu.co>
2019-06-03 16:23:44 -0500

    Create post working

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,16 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid'
-import HomePosts from './HomePosts';
+import HomePosts from './HomePosts'
 
-class Home extends React.Component {
-  render () {
-    const userId = this.props.match.params.id
-    return (
-      <div>
-        <Grid container justify='center'>
-          <HomePosts id={userId}/>
-        </Grid>
-      </div>
-    )
-  }
+const Home = (props) => {  
+  const userId = props.match.params.id
+  return (
+    <div>
+      <Grid container justify='center'>
+        <HomePosts id={userId}/>
+      </Grid>
+    </div>
+  )
 }
 
 export default Home
