a2306ebe7c17aed1e80b2778ca5baa6d39461758
judmorenomo <37850707+judmorenomo@users.noreply.github.com>
2019-06-04 02:54:54 -0500

    Login fix (#8)
    * Login probably finished
    
    * Remove console log

--- src/components/VerifyLogin.js
+++ src/components/VerifyLogin.js
@@ -2,32 +2,33 @@ import React from 'react';
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
 import Home from './Home'
+import Login from './Login'
 
 class VerifyLogin extends React.Component {  
   render(){
-    const userId = "6b8cc5c8-72bd-48b2-bbe6-efc536e8f90f"
     const email = this.props.match.params.email
     const pass = this.props.match.params.pass
-    console.log(email)
-    console.log(pass)
     return(
       <Query
         query={gql`
         {
-            profileFeedForUser(id: "${userId}"){
-              content
+            UserSess(user:{
+              email: "${email}"
+              password: "${pass}"
+            }){
               id
-              idCreator
-              createdAt
+              session
             }
           }
         `}
       >
         {({ loading, error, data }) => {
-          if (loading) return <p>Loading...</p>;
-          if (error) return <p>Error :(</p>;
-            console.log(data.profileFeedForUser)
-            return <Home id='6b8cc5c8-72bd-48b2-bbe6-efc536e8f90f'/>
+          if (loading) return <p>Loading...</p>
+          if (error) return <Login />
+            if(data.UserSess.session) {
+              return <Home id={data.UserSess.id}/>
+            }
+            return <Login />
         }}
       </Query>
     )
