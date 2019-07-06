c119fb67fb8051ab265540a321b17aa43731923b
Diego Niquefa <niquefa.diego@gmail.com>
2019-07-06 18:28:13 -0500

    get firebase token, send it to backend and show firebase notification with console.log

--- src/components/HomePosts.js
+++ src/components/HomePosts.js
@@ -1,18 +1,41 @@
 import React from 'react'
-import { Query } from 'react-apollo'
+import { Query, Mutation, graphql } from 'react-apollo'
 import gql from 'graphql-tag'
 import Post from './Post'
 import { messaging } from "../init-fcm";
+import axios from 'axios';
+import { print } from 'graphql';
+
+const ADD_TOKEN = gql`
+mutation($userId: String!, $token: String!) {
+  addToken(token:{user_id: $userId, token: $token}) {
+    user_id
+    tokens
+  }
+}`;
 
 class HomePosts extends React.Component {
   async componentDidMount() {
+    console.log("on componnentDidMount()");
     messaging.requestPermission()
       .then(async function() {
-      const token = await messaging.getToken();
-      console.log(`Token is ${token}`);
-    })
-     .catch(function(err) {
-    console.log("Unable to get permission to notify.", err);
+        const token = await messaging.getToken();
+        console.log(`Token is ${token}`);
+        const userId = localStorage.getItem('userID');
+  
+
+        console.log(ADD_TOKEN);
+
+        axios
+        .post("http://35.232.95.82/graphql", {
+          query: print(ADD_TOKEN),
+          variables: {
+            userId: userId,
+            token: token,
+          },
+        })
+        .then(res => console.log(res))
+        .catch(err => console.log(err));
     });
     navigator.serviceWorker.addEventListener("message", (message) => console.log(message));
   }
@@ -48,4 +71,5 @@ class HomePosts extends React.Component {
     )
   }
 }
+
 export default HomePosts;
