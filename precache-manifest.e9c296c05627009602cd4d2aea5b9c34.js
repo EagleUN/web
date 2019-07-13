d4772fa226ece68f81da4e4de463c55d55c7d167
Christian <cdsanabriar@unal.edu.co>
2019-07-13 11:01:16 -0500

    Updates

---
+++ precache-manifest.e9c296c05627009602cd4d2aea5b9c34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6217d4bf9cac4f4450f2b17ac54683b",
+    "url": "/web/index.html"
+  },
+  {
+    "revision": "21b446a51e5c86ccb903",
+    "url": "/web/static/css/main.1ae7b69f.chunk.css"
+  },
+  {
+    "revision": "0decb5f4f92bccb0bdf5",
+    "url": "/web/static/js/2.5302df75.chunk.js"
+  },
+  {
+    "revision": "21b446a51e5c86ccb903",
+    "url": "/web/static/js/main.d9d5160b.chunk.js"
+  },
+  {
+    "revision": "33ed4ec846e9d84e8327",
+    "url": "/web/static/js/runtime~main.c75ffd02.js"
+  },
+  {
+    "revision": "7dece5d0d1b374696c162085125c34f4",
+    "url": "/web/static/media/eagle.7dece5d0.svg"
+  }
+]);
